9a6c4d9c06db38ee21a22fca96e988ed4fe92d5a
NotHydra <rizky.irswanda115@gmail.com>
2023-12-15 12:19:35 +0800

    feat: student

--- pre/learn/src/Main.java
+++ pre/learn/src/Main.java
@@ -7,12 +7,27 @@ public class Main {
                 new Human("Human 3", 20, 175, 65),
         };
 
-        for (Human human : humans) {
-            human.greet();
-            human.data();
+        Student[] students = {
+                new Student("Student 1", 13, 150, 50, new int[] { 70, 80, 95 }),
+                new Student("Student 2", 15, 165, 60, new int[] { 76, 81, 93 }),
+                new Student("Student 3", 15, 160, 55, new int[] { 73, 83, 100 }),
+                new Student("Student 4", 12, 140, 40, new int[] { 100, 100, 100 }),
+        };
+
+        // for (Human human : humans) {
+        // human.greet();
+        // System.out.println();
+        // }
+
+        for (Student student : students) {
+            System.out.println("Name    : " + student.name);
+            System.out.println("Grade   :");
+
+            student.displayGrade();
+            System.out.println("Average : " + student.getAverage());
+            System.out.println("Index   : " + student.getIndex());
+
             System.out.println();
         }
-
-        Human.displayCount();
     }
 }
---
+++ pre/learn/src/Student.java
@@ -0,0 +1,59 @@
+public class Student extends Human {
+    static int count = 0;
+
+    int[] grades;
+
+    Student(String name, int age, int height, int weight, int[] grades) {
+        super(name, age, height, weight);
+
+        this.grades = grades;
+
+        count += 1;
+    };
+
+    static void displayCount() {
+        System.out.println("There is a total of " + count + " student object");
+    }
+
+    @Override
+    void greet() {
+        System.out.println(
+                "Hello my name is " + this.name + ", my age is " + this.age + " and I'm a student");
+    };
+
+    void displayGrade() {
+        for (int index = 0; index < this.grades.length; index += 1) {
+            System.out.println((index + 1) + ". " + this.grades[index]);
+        }
+    };
+
+    float getAverage() {
+        int total = 0;
+
+        for (int grade : grades) {
+            total += grade;
+        }
+
+        return ((float) (total / grades.length));
+    };
+
+    String getIndex() {
+        float average = getAverage();
+
+        if (86 <= average && average <= 100) {
+            return "A";
+        } else if (76 <= average && average < 86) {
+            return "B";
+        } else if (66 <= average && average < 76) {
+            return "B";
+        } else if (56 <= average && average < 66) {
+            return "C";
+        } else if (46 <= average && average < 56) {
+            return "D";
+        } else if (0 <= average && average < 46) {
+            return "E";
+        }
+
+        return "Not Valid";
+    };
+};
