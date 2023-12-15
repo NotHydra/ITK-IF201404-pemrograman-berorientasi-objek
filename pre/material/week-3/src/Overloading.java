805c6fa82658acf263a665115f684e45f6be80b7
NotHydra <rizky.irswanda115@gmail.com>
2023-12-15 18:42:54 +0800

    feat: week 3 overloading

---
+++ pre/material/week-3/src/Overloading.java
@@ -0,0 +1,13 @@
+public class Overloading {
+    public static float average(int firstValue, int secondValue) {
+        return (float) ((firstValue + secondValue) / 2);
+    };
+
+    public static float average(int firstValue, int secondValue, int thirdValue) {
+        return (float) ((firstValue + secondValue + thirdValue) / 3);
+    };
+
+    public static float average(int firstValue, int secondValue, int thirdValue, int fourthValue) {
+        return (float) ((firstValue + secondValue + thirdValue + fourthValue) / 4);
+    };
+}
