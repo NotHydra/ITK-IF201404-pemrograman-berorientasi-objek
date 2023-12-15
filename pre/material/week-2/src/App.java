ece654ca6a44c18d5f22e909f0f37ed14ec0535d
NotHydra <rizky.irswanda115@gmail.com>
2023-12-15 18:09:58 +0800

    feat: week 2 rectangle

---
+++ pre/material/week-2/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Rectangle rectangle = new Rectangle(4, 3);
+
+        System.out.println(rectangle.distance());
+    }
+}
