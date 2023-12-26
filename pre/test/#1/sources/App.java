369eab070c8802c11d71b0ab4c29a0923cc25140
NotHydra <rizky.irswanda115@gmail.com>
2023-12-26 22:15:50 +0800

    feat: add student with controller

--- pre/test/#1/sources/App.java
+++ pre/test/#1/sources/App.java
@@ -1,14 +1,13 @@
 import controllers.StudentController;
-import models.StudentModel;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        StudentModel.addStudent(new StudentModel("Student 1", 1, "Grade 1", "Major 1"));
-        StudentModel.addStudent(new StudentModel("Student 2", 2, "Grade 2", "Major 2"));
-        StudentModel.addStudent(new StudentModel("Student 3", 3, "Grade 3", "Major 3"));
-
         StudentController studentController = new StudentController();
 
-        studentController.displayStudentName();
+        studentController.addStudent("Student 1", 1, "Grade 1", "Major 1");
+        studentController.addStudent("Student 2", 2, "Grade 2", "Major 2");
+        studentController.addStudent("Student 3", 3, "Grade 3", "Major 3");
+
+        studentController.displayStudent();
     }
 }
--- pre/test/#1/sources/controllers/StudentController.java
+++ pre/test/#1/sources/controllers/StudentController.java
@@ -4,9 +4,13 @@ import models.StudentModel;
 import views.StudentView;
 
 public class StudentController {
-    public void displayStudentName() {
+    public void displayStudent() {
         for (StudentModel student : StudentModel.getStudent()) {
             StudentView.display(student.getName());
         }
     };
+
+    public void addStudent(String name, int age, String grade, String major) {
+        StudentModel.addStudent(new StudentModel(name, age, grade, major));
+    }
 }
