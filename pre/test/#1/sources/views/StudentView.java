72ebf028725d595a58ee78221a52a16222d4e725
NotHydra <rizky.irswanda115@gmail.com>
2023-12-26 21:54:33 +0800

    refactor: static view

--- pre/test/#1/sources/views/StudentView.java
+++ pre/test/#1/sources/views/StudentView.java
@@ -1,10 +1,7 @@
 package views;
 
 public class StudentView {
-    public StudentView() {
-    };
-
-    public void Display(String text) {
+    public static void Display(String text) {
         System.out.println(text);
     };
 }
