6b38e8a4216cbff53c037db269e6400d73a8a945
NotHydra <rizky.irswanda115@gmail.com>
2023-12-26 22:11:38 +0800

    feat: implement student

--- pre/test/#1/sources/App.java
+++ pre/test/#1/sources/App.java
@@ -1,26 +1,14 @@
+import controllers.StudentController;
 import models.StudentModel;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        StudentModel student1 = new StudentModel("Student 1", 1, "Grade 1", "Major 1");
-        StudentModel student2 = new StudentModel("Student 2", 2, "Grade 2", "Major 2");
-        StudentModel student3 = new StudentModel("Student 3", 3, "Grade 3", "Major 3");
+        StudentModel.addStudent(new StudentModel("Student 1", 1, "Grade 1", "Major 1"));
+        StudentModel.addStudent(new StudentModel("Student 2", 2, "Grade 2", "Major 2"));
+        StudentModel.addStudent(new StudentModel("Student 3", 3, "Grade 3", "Major 3"));
 
-        System.out.println(StudentModel.getStudent());
-        StudentModel.addStudent(student1);
+        StudentController studentController = new StudentController();
 
-        System.out.println(StudentModel.getStudent());
-        StudentModel.addStudent(student2);
-
-        System.out.println(StudentModel.getStudent());
-        StudentModel.addStudent(student3);
-
-        System.out.println(StudentModel.getStudent());
-        StudentModel.removeStudent(student2);
-
-        System.out.println(StudentModel.getStudent());
-        StudentModel.changeStudent(student1, student2);
-
-        System.out.println(StudentModel.getStudent());
+        studentController.displayStudentName();
     }
 }
