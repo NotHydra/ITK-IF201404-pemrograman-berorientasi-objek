79a3a031045e705ec9f67eede84d2d3b3180d4ac
NotHydra <rizky.irswanda115@gmail.com>
2023-12-27 20:10:14 +0800

    feat: env

---
+++ pre/test/#2/sources/Dependency.java
@@ -0,0 +1,27 @@
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class Dependency {
+    private String databaseURL;
+    private String databaseUsername;
+    private String databasePassword;
+
+    public Dependency() {
+        Dotenv environment = Dotenv.load();
+
+        this.databaseURL = environment.get("DATABASE_URL");
+        this.databaseUsername = environment.get("DATABASE_USERNAME");
+        this.databasePassword = environment.get("DATABASE_PASSWORD");
+    }
+
+    public String getDatabaseURL() {
+        return this.databaseURL;
+    }
+
+    public String getDatabaseUsername() {
+        return this.databaseUsername;
+    }
+
+    public String getDatabasePassword() {
+        return this.databasePassword;
+    }
+}
