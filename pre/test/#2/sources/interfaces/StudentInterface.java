9df8fe9b23104f5d7cd0dfd04da347c68adb3677
NotHydra <rizky.irswanda115@gmail.com>
2023-12-27 16:03:40 +0800

    feat: student interface

---
+++ pre/test/#2/sources/interfaces/StudentInterface.java
@@ -0,0 +1,21 @@
+package interfaces;
+
+public interface StudentInterface {
+    public int getId();
+
+    public String getName();
+
+    public String getGrade();
+
+    public String getMajor();
+
+    public void setId(int id);
+
+    public void setName(String name);
+
+    public void setGrade(String grade);
+
+    public void setMajor(String major);
+
+    public String toString();
+}
