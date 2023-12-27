8a2058af80962847993a4bd703b1b8f3b9028d84
NotHydra <rizky.irswanda115@gmail.com>
2023-12-27 15:34:38 +0800

    feat: student schema

---
+++ pre/test/#2/sources/schemas/StudentSchema.java
@@ -0,0 +1,47 @@
+package schemas;
+
+public class StudentSchema {
+    private int id;
+    private String name;
+    private String grade;
+    private String major;
+
+    public StudentSchema(int id, String name, String grade, String major) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+        this.major = major;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getGrade() {
+        return this.grade;
+    }
+
+    public String getMajor() {
+        return this.major;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+}
