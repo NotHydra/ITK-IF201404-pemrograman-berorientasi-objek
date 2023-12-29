7ee3a408c96220062bc954f79b4d1ba070d77d09
NotHydra <rizky.irswanda115@gmail.com>
2023-12-29 18:33:38 +0800

    feat: base model abstract
    Signed-off-by: NotHydra <rizky.irswanda115@gmail.com>

---
+++ pre/test/#2/sources/models/BaseModel.java
@@ -0,0 +1,16 @@
+package models;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public abstract class BaseModel<SchemaType> {
+    public abstract ArrayList<SchemaType> get() throws SQLException;
+
+    public abstract SchemaType getOne(int id) throws SQLException;
+
+    public abstract void add(SchemaType schema) throws SQLException;
+
+    public abstract void change(int id, SchemaType schema) throws SQLException;
+
+    public abstract void remove(int id) throws SQLException;
+}
