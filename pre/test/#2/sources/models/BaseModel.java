9b30f7be819376325d702f9fcefae666f433a349
NotHydra <rizky.irswanda115@gmail.com>
2023-12-31 06:55:21 +0800

    feat: model error handling

--- pre/test/#2/sources/models/BaseModel.java
+++ pre/test/#2/sources/models/BaseModel.java
@@ -1,17 +1,15 @@
 package models;
 
-import java.sql.SQLException;
-
 public abstract class BaseModel<SchemaType> {
-    public abstract SchemaType[] get() throws SQLException;
+    public abstract SchemaType[] get();
 
-    public abstract SchemaType getOne(int id) throws SQLException;
+    public abstract SchemaType getOne(int id);
 
-    public abstract void add(SchemaType schema) throws SQLException;
+    public abstract void add(SchemaType schema);
 
-    public abstract void add(SchemaType[] schemas) throws SQLException;
+    public abstract void add(SchemaType[] schemas);
 
-    public abstract void change(int id, SchemaType schema) throws SQLException;
+    public abstract void change(int id, SchemaType schema);
 
-    public abstract void remove(int id) throws SQLException;
+    public abstract void remove(int id);
 }
--- pre/test/#2/sources/models/StudentModel.java
+++ pre/test/#2/sources/models/StudentModel.java
@@ -7,106 +7,134 @@ import schemas.StudentSchema;
 
 public class StudentModel extends BaseModel<StudentSchema> {
     @Override
-    public StudentSchema[] get() throws SQLException {
-        final Database database = new Database();
-        final int total = database.tableTotal("student");
-        final ResultSet result = database.executeQuery("SELECT id, name, grade, major FROM student;");
+    public StudentSchema[] get() {
+        try {
+            final Database database = new Database();
+            final int total = database.tableTotal("student");
+            final ResultSet result = database.executeQuery("SELECT id, name, grade, major FROM student;");
 
-        final StudentSchema[] students = new StudentSchema[total];
-        int i = 0;
-        while (result.next()) {
-            students[i] = new StudentSchema(
-                    result.getInt("id"),
-                    result.getString("name"),
-                    result.getString("grade"),
-                    result.getString("major"));
+            final StudentSchema[] students = new StudentSchema[total];
+            int i = 0;
+            while (result.next()) {
+                students[i] = new StudentSchema(
+                        result.getInt("id"),
+                        result.getString("name"),
+                        result.getString("grade"),
+                        result.getString("major"));
 
-            i++;
-        }
-
-        database.close();
-
-        return students;
-    }
-
-    @Override
-    public StudentSchema getOne(int id) throws SQLException {
-        final Database database = new Database();
-        final ResultSet result = database
-                .executeQuery("SELECT id, name, grade, major FROM student WHERE id='" + id + "';");
-
-        StudentSchema student = null;
-
-        if (result.next()) {
-            student = new StudentSchema(
-                    result.getInt("id"),
-                    result.getString("name"),
-                    result.getString("grade"),
-                    result.getString("major"));
-        }
-
-        database.close();
-
-        return student;
-    }
-
-    @Override
-    public void add(StudentSchema schema) throws SQLException {
-        final Database database = new Database();
-        database.executeUpdate(
-                "INSERT INTO student (name, grade, major) VALUES ("
-                        + "'" + schema.getName() + "', "
-                        + "'" + schema.getGrade() + "', "
-                        + "'" + schema.getMajor() + "'"
-                        + ");");
-
-        database.close();
-    }
-
-    @Override
-    public void add(StudentSchema[] schemas) throws SQLException {
-        final Database database = new Database();
-
-        String query = "INSERT INTO student (name, grade, major) VALUES ";
-        for (int i = 0; i < schemas.length; i++) {
-            query = query + "("
-                    + "'" + schemas[i].getName() + "', "
-                    + "'" + schemas[i].getGrade() + "', "
-                    + "'" + schemas[i].getMajor() + "'"
-                    + ")";
-
-            if (i != (schemas.length - 1)) {
-                query = query + ", ";
-            } else {
-                query = query + ";";
+                i++;
             }
+
+            database.close();
+
+            return students;
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-        database.executeUpdate(query);
-
-        database.close();
+        return null;
     }
 
     @Override
-    public void change(int id, StudentSchema schema) throws SQLException {
-        final Database database = new Database();
-        database.executeUpdate(
-                "UPDATE student SET "
-                        + "name='" + schema.getName() + "', "
-                        + "grade='" + schema.getGrade() + "', "
-                        + "major='" + schema.getMajor() + "' "
-                        + "WHERE "
-                        + "id='" + id + "'"
-                        + ";");
+    public StudentSchema getOne(int id) {
+        try {
+            final Database database = new Database();
+            final ResultSet result = database
+                    .executeQuery("SELECT id, name, grade, major FROM student WHERE id='" + id + "';");
 
-        database.close();
+            StudentSchema student = null;
+
+            if (result.next()) {
+                student = new StudentSchema(
+                        result.getInt("id"),
+                        result.getString("name"),
+                        result.getString("grade"),
+                        result.getString("major"));
+            }
+
+            database.close();
+
+            return student;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
     @Override
-    public void remove(int id) throws SQLException {
-        final Database database = new Database();
-        database.executeUpdate("DELETE FROM student WHERE id='" + id + "'");
+    public void add(StudentSchema schema) {
+        try {
+            final Database database = new Database();
+            database.executeUpdate(
+                    "INSERT INTO student (name, grade, major) VALUES ("
+                            + "'" + schema.getName() + "', "
+                            + "'" + schema.getGrade() + "', "
+                            + "'" + schema.getMajor() + "'"
+                            + ");");
 
-        database.close();
+            database.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void add(StudentSchema[] schemas) {
+        try {
+            final Database database = new Database();
+
+            String query = "INSERT INTO student (name, grade, major) VALUES ";
+            for (int i = 0; i < schemas.length; i++) {
+                query = query + "("
+                        + "'" + schemas[i].getName() + "', "
+                        + "'" + schemas[i].getGrade() + "', "
+                        + "'" + schemas[i].getMajor() + "'"
+                        + ")";
+
+                if (i != (schemas.length - 1)) {
+                    query = query + ", ";
+                } else {
+                    query = query + ";";
+                }
+            }
+
+            database.executeUpdate(query);
+
+            database.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void change(int id, StudentSchema schema) {
+        try {
+            final Database database = new Database();
+            database.executeUpdate(
+                    "UPDATE student SET "
+                            + "name='" + schema.getName() + "', "
+                            + "grade='" + schema.getGrade() + "', "
+                            + "major='" + schema.getMajor() + "' "
+                            + "WHERE "
+                            + "id='" + id + "'"
+                            + ";");
+
+            database.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void remove(int id) {
+        try {
+            final Database database = new Database();
+            database.executeUpdate("DELETE FROM student WHERE id='" + id + "'");
+
+            database.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
