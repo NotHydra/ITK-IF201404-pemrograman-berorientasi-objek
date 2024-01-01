0d43adc64b6617a980f2af0eac3b364c69d96b84
NotHydra <rizky.irswanda115@gmail.com>
2024-01-02 05:16:13 +0800

    feat: javafx iniatialize

--- pre/test/#2/sources/App.java
+++ pre/test/#2/sources/App.java
@@ -9,30 +9,30 @@ public class App {
         StudentView studentView = new StudentView();
         StudentController studentController = new StudentController(studentModel, studentView);
 
-        // Controller Implementation
-        for (StudentSchema student : studentController.get()) {
-            System.out.println(student);
-        }
+        // // Controller Implementation
+        // for (StudentSchema student : studentController.get()) {
+        // System.out.println(student);
+        // }
 
-        System.out.println(studentController.getOne(1));
+        // System.out.println(studentController.getOne(1));
 
-        studentController.add("Student 4", "Grade 4", "Major 4");
+        // studentController.add("Student 4", "Grade 4", "Major 4");
 
-        studentController.add(new StudentSchema[] {
-                new StudentSchema("Student 5", "Grade 5", "Major 5"),
-                new StudentSchema("Student 6", "Grade 6", "Major 6"),
-                new StudentSchema("Student 7", "Grade 7", "Major 7")
-        });
+        // studentController.add(new StudentSchema[] {
+        // new StudentSchema("Student 5", "Grade 5", "Major 5"),
+        // new StudentSchema("Student 6", "Grade 6", "Major 6"),
+        // new StudentSchema("Student 7", "Grade 7", "Major 7")
+        // });
 
-        studentController.change(1, "Student 5", "Grade 5", "Major 5");
+        // studentController.change(1, "Student 5", "Grade 5", "Major 5");
 
-        studentController.remove(3);
+        // studentController.remove(3);
 
-        // Model Implementation
-        try {
-            System.out.println(new StudentSchema(1, "Student 1", "Grade 1", "Major 1"));
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+        // // Model Implementation
+        // try {
+        // System.out.println(new StudentSchema(1, "Student 1", "Grade 1", "Major 1"));
+        // } catch (Exception e) {
+        // System.out.println(e.getMessage());
+        // }
     };
 }
