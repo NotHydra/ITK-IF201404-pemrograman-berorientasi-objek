0c6ebc11adf768ee1d4372f8f09bc549f71606e8
NotHydra <rizky.irswanda115@gmail.com>
2024-01-06 02:24:11 +0800

    feat: dosen extend model

---
+++ pre/tugas-besar/sources/interfaces/DosenExtendInterface.java
@@ -0,0 +1,13 @@
+package interfaces;
+
+import models.TempatLahirModel;
+import models.PendidikanModel;
+import models.ProgramStudiExtendModel;
+
+public interface DosenExtendInterface {
+    public TempatLahirModel getTempatLahirExtend();
+
+    public PendidikanModel getPendidikanExtend();
+
+    public ProgramStudiExtendModel getProgramStudiExtend();
+}
---
+++ pre/tugas-besar/sources/models/DosenExtendModel.java
@@ -0,0 +1,104 @@
+package models;
+
+import interfaces.DosenExtendInterface;
+import utilities.CustomDate;
+
+public class DosenExtendModel extends DosenModel implements DosenExtendInterface {
+    private final TempatLahirModel tempatLahirExtend;
+    private final PendidikanModel pendidikanExtend;
+    private final ProgramStudiExtendModel programStudiExtend;
+
+    public DosenExtendModel(
+            int id,
+            String nik,
+            String nip,
+            String nama,
+            String email,
+            String password,
+            String alamat,
+            int idTempatLahir,
+            String tanggalLahir,
+            String jenisKelamin,
+            String golonganDarah,
+            String agama,
+            String nomorTelepon,
+            int idPendidikan,
+            int idProgramStudi,
+            boolean aktif,
+            String keterangan,
+            String tempatLahirTempatLahir,
+            String pendidikanPendidikan,
+            String pendidikanSingkatan,
+            int programStudiIdJurusan,
+            String programStudiProgramStudi,
+            String programStudiDeskripsi,
+            String jurusanJurusan,
+            String jurusanDeskripsi) {
+        super(
+                id,
+                nik,
+                nip,
+                nama,
+                email,
+                password,
+                alamat,
+                idTempatLahir,
+                tanggalLahir,
+                jenisKelamin,
+                golonganDarah,
+                agama,
+                nomorTelepon,
+                idPendidikan,
+                idProgramStudi,
+                aktif,
+                keterangan);
+
+        this.tempatLahirExtend = new TempatLahirModel(idTempatLahir, tempatLahirTempatLahir);
+        this.pendidikanExtend = new PendidikanModel(idPendidikan, pendidikanPendidikan, pendidikanSingkatan);
+        this.programStudiExtend = new ProgramStudiExtendModel(
+                idProgramStudi,
+                programStudiIdJurusan,
+                programStudiProgramStudi,
+                programStudiDeskripsi,
+                jurusanJurusan,
+                jurusanDeskripsi);
+    }
+
+    public TempatLahirModel getTempatLahirExtend() {
+        return this.tempatLahirExtend;
+    }
+
+    public PendidikanModel getPendidikanExtend() {
+        return this.pendidikanExtend;
+    }
+
+    public ProgramStudiExtendModel getProgramStudiExtend() {
+        return this.programStudiExtend;
+    }
+
+    @Override
+    public String toString() {
+        return "Mahasiswa("
+                + "id=" + this.getId() + ", "
+                + "nik=" + this.getNIK() + ", "
+                + "nim=" + this.getNIP() + ", "
+                + "nama=" + this.getNama() + ", "
+                + "email=" + this.getEmail() + ", "
+                + "password=" + this.getPassword() + ", "
+                + "alamat=" + this.getAlamat() + ", "
+                + "idTempatLahir=" + this.getIdTempatLahir() + ", "
+                + "tanggalLahir=" + CustomDate.dateToString(this.getTanggalLahir()) + ", "
+                + "jenisKelamin=" + this.getJenisKelamin() + ", "
+                + "golonganDarah=" + this.getGolonganDarah() + ", "
+                + "agama=" + this.getAgama() + ", "
+                + "nomorTelepon=" + this.getNomorTelepon() + ", "
+                + "idPendidikan=" + this.getIdPendidikan() + ", "
+                + "idProgramStudi=" + this.getIdProgramStudi() + ", "
+                + "aktif=" + this.getAktif() + ", "
+                + "keterangan=" + this.getKeterangan() + ", "
+                + this.getTempatLahirExtend().toString() + ", "
+                + this.getPendidikanExtend().toString() + ", "
+                + this.getProgramStudiExtend().toString()
+                + ")";
+    }
+}
