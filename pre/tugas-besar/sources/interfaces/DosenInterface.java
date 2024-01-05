77b79a90baa80b9b33d84aa76ece40acdeeadaa7
NotHydra <rizky.irswanda115@gmail.com>
2024-01-06 01:13:21 +0800

    feat: dosen model

---
+++ pre/tugas-besar/sources/interfaces/DosenInterface.java
@@ -0,0 +1,37 @@
+package interfaces;
+
+import java.util.Date;
+
+public interface DosenInterface {
+    public String getNIK();
+
+    public String getNIP();
+
+    public String getNama();
+
+    public String getEmail();
+
+    public String getPassword();
+
+    public String getAlamat();
+
+    public int getIdTempatLahir();
+
+    public Date getTanggalLahir();
+
+    public String getJenisKelamin();
+
+    public String getGolonganDarah();
+
+    public String getAgama();
+
+    public String getNomorTelepon();
+
+    public int getIdPendidikan();
+
+    public int getIdProgramStudi();
+
+    public boolean getAktif();
+
+    public String getKeterangan();
+}
---
+++ pre/tugas-besar/sources/models/DosenModel.java
@@ -0,0 +1,320 @@
+package models;
+
+import java.util.Date;
+
+import interfaces.DosenInterface;
+
+import utilities.CustomDate;
+
+public class DosenModel extends BaseModel implements DosenInterface {
+    private final int id;
+    private final String nik;
+    private final String nip;
+    private final String nama;
+    private final String email;
+    private final String password;
+    private final String alamat;
+    private final int idTempatLahir;
+    private final Date tanggalLahir;
+    private final String jenisKelamin;
+    private final String golonganDarah;
+    private final String agama;
+    private final String nomorTelepon;
+    private final int idPendidikan;
+    private final int idProgramStudi;
+    private final boolean aktif;
+    private final String keterangan;
+
+    public DosenModel(
+            String nik,
+            String nip,
+            String nama,
+            String email,
+            String password,
+            String alamat,
+            int idTempatLahir,
+            String tanggalLahir,
+            String jenisKelamin,
+            String golonganDarah,
+            String agama,
+            String nomorTelepon,
+            int idPendidikan,
+            int idProgramStudi,
+            boolean aktif,
+            String keterangan) {
+        validate(
+                nik,
+                nip,
+                nama,
+                email,
+                password,
+                alamat,
+                idTempatLahir,
+                tanggalLahir,
+                jenisKelamin,
+                golonganDarah,
+                agama,
+                nomorTelepon,
+                idPendidikan,
+                idProgramStudi);
+
+        this.id = -1;
+        this.nik = nik;
+        this.nip = nip;
+        this.nama = nama;
+        this.email = email;
+        this.password = password;
+        this.alamat = alamat;
+        this.idTempatLahir = idTempatLahir;
+        this.tanggalLahir = CustomDate.sqlToDate(tanggalLahir);
+        this.jenisKelamin = jenisKelamin;
+        this.golonganDarah = golonganDarah;
+        this.agama = agama;
+        this.nomorTelepon = nomorTelepon;
+        this.idPendidikan = idPendidikan;
+        this.idProgramStudi = idProgramStudi;
+        this.aktif = aktif;
+        this.keterangan = keterangan;
+    }
+
+    public DosenModel(
+            int id,
+            String nik,
+            String nip,
+            String nama,
+            String email,
+            String password,
+            String alamat,
+            int idTempatLahir,
+            String tanggalLahir,
+            String jenisKelamin,
+            String golonganDarah,
+            String agama,
+            String nomorTelepon,
+            int idPendidikan,
+            int idProgramStudi,
+            boolean aktif,
+            String keterangan) {
+        if (id <= 0) {
+            throw new IllegalArgumentException("Id is invalid");
+        }
+
+        validate(
+                nik,
+                nip,
+                nama,
+                email,
+                password,
+                alamat,
+                idTempatLahir,
+                tanggalLahir,
+                jenisKelamin,
+                golonganDarah,
+                agama,
+                nomorTelepon,
+                idPendidikan,
+                idProgramStudi);
+
+        this.id = id;
+        this.nik = nik;
+        this.nip = nip;
+        this.nama = nama;
+        this.email = email;
+        this.password = password;
+        this.alamat = alamat;
+        this.idTempatLahir = idTempatLahir;
+        this.tanggalLahir = CustomDate.sqlToDate(tanggalLahir);
+        this.jenisKelamin = jenisKelamin;
+        this.golonganDarah = golonganDarah;
+        this.agama = agama;
+        this.nomorTelepon = nomorTelepon;
+        this.idPendidikan = idPendidikan;
+        this.idProgramStudi = idProgramStudi;
+        this.aktif = aktif;
+        this.keterangan = keterangan;
+    }
+
+    private void validate(
+            String nik,
+            String nip,
+            String nama,
+            String email,
+            String password,
+            String alamat,
+            int idTempatLahir,
+            String tanggalLahir,
+            String jenisKelamin,
+            String golonganDarah,
+            String agama,
+            String nomorTelepon,
+            int idPendidikan,
+            int idProgramStudi) {
+        if (nik == null || nik.trim().isEmpty()) {
+            throw new IllegalArgumentException("NIK cannot be empty");
+        }
+
+        if (nip == null || nip.trim().isEmpty()) {
+            throw new IllegalArgumentException("NIP cannot be empty");
+        }
+
+        if (nama == null || nama.trim().isEmpty()) {
+            throw new IllegalArgumentException("Nama cannot be empty");
+        }
+
+        if (email == null || email.trim().isEmpty()) {
+            throw new IllegalArgumentException("Email cannot be empty");
+        }
+
+        if (password == null || password.trim().isEmpty()) {
+            throw new IllegalArgumentException("Password cannot be empty");
+        }
+
+        if (alamat == null || alamat.trim().isEmpty()) {
+            throw new IllegalArgumentException("Alamat cannot be empty");
+        }
+
+        if (idTempatLahir <= 0) {
+            throw new IllegalArgumentException("Id tempat lahir is invalid");
+        }
+
+        if (tanggalLahir == null || tanggalLahir.trim().isEmpty()) {
+            throw new IllegalArgumentException("Tanggal lahir cannot be empty");
+        }
+
+        if (!tanggalLahir.matches("\\d{4}-\\d{2}-\\d{2}")) {
+            throw new IllegalArgumentException("Tanggal lahir invalid date format.");
+        }
+
+        if (jenisKelamin == null || jenisKelamin.trim().isEmpty()) {
+            throw new IllegalArgumentException("Jenis kelamin cannot be empty");
+        }
+
+        if (golonganDarah == null || golonganDarah.trim().isEmpty()) {
+            throw new IllegalArgumentException("Golongan darah cannot be empty");
+        }
+
+        if (agama == null || agama.trim().isEmpty()) {
+            throw new IllegalArgumentException("Agama cannot be empty");
+        }
+
+        if (nomorTelepon == null || nomorTelepon.trim().isEmpty()) {
+            throw new IllegalArgumentException("Nomor telepon cannot be empty");
+        }
+
+        if (idPendidikan <= 0) {
+            throw new IllegalArgumentException("Id pendidikan is invalid");
+        }
+
+        if (idProgramStudi <= 0) {
+            throw new IllegalArgumentException("Id program studi is invalid");
+        }
+    }
+
+    @Override
+    public int getId() {
+        return this.id;
+    }
+
+    @Override
+    public String getNIK() {
+        return nik;
+    }
+
+    @Override
+    public String getNIP() {
+        return nip;
+    }
+
+    @Override
+    public String getNama() {
+        return nama;
+    }
+
+    @Override
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getAlamat() {
+        return alamat;
+    }
+
+    @Override
+    public int getIdTempatLahir() {
+        return idTempatLahir;
+    }
+
+    @Override
+    public Date getTanggalLahir() {
+        return tanggalLahir;
+    }
+
+    @Override
+    public String getJenisKelamin() {
+        return jenisKelamin;
+    }
+
+    @Override
+    public String getGolonganDarah() {
+        return golonganDarah;
+    }
+
+    @Override
+    public String getAgama() {
+        return agama;
+    }
+
+    @Override
+    public String getNomorTelepon() {
+        return nomorTelepon;
+    }
+
+    @Override
+    public int getIdPendidikan() {
+        return idPendidikan;
+    }
+
+    @Override
+    public int getIdProgramStudi() {
+        return idProgramStudi;
+    }
+
+    @Override
+    public boolean getAktif() {
+        return aktif;
+    }
+
+    @Override
+    public String getKeterangan() {
+        return keterangan;
+    }
+
+    @Override
+    public String toString() {
+        return "Dosen("
+                + "id=" + id + ", "
+                + "nik=" + nik + ", "
+                + "nip=" + nip + ", "
+                + "nama=" + nama + ", "
+                + "email=" + email + ", "
+                + "password=" + password + ", "
+                + "alamat=" + alamat + ", "
+                + "idTempatLahir=" + idTempatLahir + ", "
+                + "tanggalLahir=" + CustomDate.dateToString(tanggalLahir) + ", "
+                + "jenisKelamin=" + jenisKelamin + ", "
+                + "golonganDarah=" + golonganDarah + ", "
+                + "agama=" + agama + ", "
+                + "nomorTelepon=" + nomorTelepon + ", "
+                + "idPendidikan=" + idPendidikan + ", "
+                + "idProgramStudi=" + idProgramStudi + ", "
+                + "aktif=" + aktif + ", "
+                + "keterangan=" + keterangan
+                + ")";
+    }
+}
