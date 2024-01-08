e1557ead5b0a7a249d444c3474402c67040f05e5
NotHydra <rizky.irswanda115@gmail.com>
2024-01-09 04:43:51 +0800

    feat: base service default methods

--- pre/tugas-besar/sources/services/BaseService.java
+++ pre/tugas-besar/sources/services/BaseService.java
@@ -1,6 +1,21 @@
 package services;
 
+import providers.Database;
+
 public abstract class BaseService<ModelType> {
+    private final String table;
+
+    public BaseService(String table) {
+        this.table = table;
+    }
+
+    public void display() {
+        for (ModelType model : this.get()) {
+            System.out.println(model);
+            System.out.println();
+        }
+    };
+
     public abstract ModelType[] get();
 
     public abstract ModelType getOne(int id);
@@ -11,5 +26,26 @@ public abstract class BaseService<ModelType> {
 
     public abstract void change(int id, ModelType model);
 
-    public abstract void remove(int id);
+    public void remove(int id) {
+        try {
+            final Database database = new Database();
+            database.executeUpdate("DELETE FROM " + table + " WHERE id='" + id + "'");
+
+            database.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void clear() {
+        try {
+            final Database database = new Database();
+            database.disableForeignKey();
+            database.executeUpdate("TRUNCATE TABLE " + this.table + ";");
+
+            database.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
