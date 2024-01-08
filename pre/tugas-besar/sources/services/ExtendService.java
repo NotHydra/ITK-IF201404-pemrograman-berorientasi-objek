8b1b6c869202ce6f5d4d4d240cca328164d17c2e
NotHydra <rizky.irswanda115@gmail.com>
2024-01-09 04:50:03 +0800

    feat: extened service display extend default method

--- pre/tugas-besar/sources/services/ExtendService.java
+++ pre/tugas-besar/sources/services/ExtendService.java
@@ -1,6 +1,13 @@
 package services;
 
 public interface ExtendService<ModelExtendType> {
+    public default void displayExtend() {
+        for (ModelExtendType modelExtend : this.getExtend()) {
+            System.out.println(modelExtend);
+            System.out.println();
+        }
+    };
+
     public ModelExtendType[] getExtend();
 
     public ModelExtendType getOneExtend(int id);
