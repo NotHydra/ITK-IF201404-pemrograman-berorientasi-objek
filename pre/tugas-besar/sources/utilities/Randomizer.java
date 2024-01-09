89f5597bcbce46756b22beea307859010a1c70ea
NotHydra <rizky.irswanda115@gmail.com>
2024-01-09 19:19:25 +0800

    feat: randomizer date

--- pre/tugas-besar/sources/utilities/Randomizer.java
+++ pre/tugas-besar/sources/utilities/Randomizer.java
@@ -1,6 +1,10 @@
 package utilities;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Randomizer {
     private static final Random random = new Random();
@@ -13,5 +17,21 @@ public class Randomizer {
 
     public static <Type> Type pickEnum(Class<Type> enumClass) {
         return enumClass.getEnumConstants()[random.nextInt(enumClass.getEnumConstants().length)];
+    }
+
+    public static String date(String start, String end) {
+        try {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+            return dateFormat.format(
+                    new Date(
+                            ThreadLocalRandom.current().nextLong(
+                                    dateFormat.parse(start).getTime(),
+                                    dateFormat.parse(end).getTime())));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
