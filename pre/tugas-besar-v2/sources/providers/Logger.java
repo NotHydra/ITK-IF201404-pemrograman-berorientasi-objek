7bc4a61e67b09c0772ae4dcd76fe1f17453fa820
NotHydra <rizky.irswanda115@gmail.com>
2024-03-13 12:33:07 +0800

    feat(tugas-besar-v2): initialize logger

---
+++ pre/tugas-besar-v2/sources/providers/Logger.java
@@ -0,0 +1,26 @@
+package providers;
+
+public class Logger {
+    private final String context;
+
+    public Logger(String context) {
+        this.context = context;
+    }
+
+    public void info(String message) {
+        System.out.println("[" + this.context + "][info]" + message);
+    }
+
+    public void debug(String message) {
+        System.out.println("[" + this.context + "][debug]" + message);
+    }
+
+    public void warn(String message) {
+        System.out.println("[" + this.context + "][warn]" + message);
+    }
+
+    public void error(String message) {
+        System.err.println("[" + this.context + "][error]" + message);
+    }
+
+}
