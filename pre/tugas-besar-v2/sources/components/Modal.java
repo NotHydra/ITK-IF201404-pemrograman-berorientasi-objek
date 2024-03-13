a9d0ecd1a3b5a3196060e51efeb6895dafeb0443
NotHydra <rizky.irswanda115@gmail.com>
2024-03-13 23:02:16 +0800

    feat(tugas-besar-v2): modal component

---
+++ pre/tugas-besar-v2/sources/components/Modal.java
@@ -0,0 +1,27 @@
+package components;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+
+public class Modal {
+    public static boolean confirmation() {
+        Alert confirmation = new Alert(Alert.AlertType.CONFIRMATION);
+        confirmation.setTitle("Notification");
+        confirmation.setHeaderText("Confirmation");
+        confirmation.setContentText("Are you sure?");
+
+        confirmation.getButtonTypes().setAll(ButtonType.YES, ButtonType.NO);
+
+        java.util.Optional<ButtonType> result = confirmation.showAndWait();
+
+        return result.isPresent() && result.get() == ButtonType.YES;
+    }
+
+    public static void fail(String message) {
+        Alert error = new Alert(Alert.AlertType.ERROR);
+        error.setTitle("Notification");
+        error.setHeaderText("Fail");
+        error.setContentText(message);
+        error.showAndWait();
+    }
+}
