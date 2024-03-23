bfa2120eae117a5d0469dc7a33f395f126152ced
NotHydra <rizky.irswanda115@gmail.com>
2024-03-23 09:57:13 +0800

    feat(week-9): more dummy data

--- task/week-9/polymorphism-oop/src/App.java
+++ task/week-9/polymorphism-oop/src/App.java
@@ -6,7 +6,14 @@ public class App {
                 "0812345678910",
                 "email1@gmail.com");
 
+        Contact contact2 = new Contact(
+                2,
+                "Name 2",
+                "0812345678911",
+                "email2@gmail.com");
+
         System.out.println(contact1);
+        System.out.println(contact2);
 
         PersonalContact personalContact1 = new PersonalContact(
                 1,
@@ -15,14 +22,28 @@ public class App {
                 "email1@gmail.com",
                 "Nickname 1");
 
+        PersonalContact personalContact2 = new PersonalContact(
+                2,
+                "Name 2",
+                "0812345678911",
+                "email2@gmail.com",
+                "Nickname 2");
+
         System.out.println(personalContact1);
+        System.out.println(personalContact2);
 
         Organization organization1 = new Organization(
                 "Organization 1",
                 "0812345678910",
                 "email1@gmail.com");
 
+        Organization organization2 = new Organization(
+                "Organization 2",
+                "0812345678911",
+                "email2@gmail.com");
+
         System.out.println(organization1);
+        System.out.println(organization2);
 
         BusinessContact businessContact1 = new BusinessContact(
                 1,
@@ -31,6 +52,14 @@ public class App {
                 "email1@gmail.com",
                 organization1);
 
+        BusinessContact businessContact2 = new BusinessContact(
+                2,
+                "Name 2",
+                "0812345678911",
+                "email2@gmail.com",
+                organization2);
+
         System.out.println(businessContact1);
+        System.out.println(businessContact2);
     }
 }
