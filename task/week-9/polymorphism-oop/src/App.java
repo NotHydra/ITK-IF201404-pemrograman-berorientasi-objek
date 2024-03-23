aaeeaf31adf3eacc4c021fc4865ca3bc60da8746
NotHydra <rizky.irswanda115@gmail.com>
2024-03-23 09:36:49 +0800

    feat(week-9): contact

---
+++ task/week-9/polymorphism-oop/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Contact contact1 = new Contact(1, "Name 1", "0812345678910", "email1@gmail.com");
+
+        System.out.println(contact1);
+
+    }
+}
---
+++ task/week-9/polymorphism-oop/src/Contact.java
@@ -0,0 +1,50 @@
+public class Contact {
+    private final int id;
+    private String name;
+    private String phoneNumber;
+    private String email;
+
+    public Contact(int id, String name, String phoneNumber, String email) {
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String toString() {
+        return "Contact("
+                + "id=" + id + ", "
+                + "name=\"" + name + "\", "
+                + "phoneNumber=\"" + phoneNumber + "\", "
+                + "email=\"" + email + "\""
+                + ")";
+    }
+}
